c836642c7f2613703fe0df4565bb4a5c9d8baad4
abhishek <shukla13122000@gmail.com>
2024-12-29 13:45:10 +0530

    resolved vite env

--- client/src/pages/CallPage.jsx
+++ client/src/pages/CallPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
-const socket = io(import.meta.VITE_REMOTE_URL);
+const socket = io(import.meta.env.VITE_REMOTE_URL);
 const CallPage = () => {
   const { roomId } = useParams;
   const localVideoRef = useRef(null);
